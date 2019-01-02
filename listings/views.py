f3e929c77ede59a10ff4322fb6e833abd584b555
Nayaya25 <ibrahimnayaya25@gmail.com>
2019-01-02 12:16:50 +0100

    Added Pagination to the listing page

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 
-def listing(request):
-    return render(request, 'listings/listing.html')
-
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html')
 
 def search(request):
     return render(request, 'listings/search.html')
