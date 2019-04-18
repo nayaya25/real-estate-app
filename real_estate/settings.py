a5360c788f2006116048e604219b470f2aa57a9f
Nayaya25 <ibrahimnayaya25@gmail.com>
2019-04-18 14:04:24 +0100

    fixed .vscode folder to gitignore

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9&utp)4j2og(j32*8@qv*ou-8_^&57kkb+@t83ne#esmq=k%9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
